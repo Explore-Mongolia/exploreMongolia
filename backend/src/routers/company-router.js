3fe4797ec3844225c31fece15bcb7960df52ac3f
24LP2287 24LP2287 <24LP2287@G301-CC0192.local>
2025-04-25 12:56:34 +0800

    destinaiton

--- backend/src/routers/company-router.js
+++ backend/src/routers/company-router.js
@@ -2,8 +2,11 @@
 import { Router } from "express";
 import { createCompany } from "../controller/company/create-company.js";
 import { getCompany } from "../controller/company/get-company.js";
+import { getAllCompanies } from "../controller/company/get-all-companies.js";
 
 export const companyRouter = Router();
 
 companyRouter.post('/create', createCompany);
-companyRouter.get('/:id', getCompany)
+companyRouter.get('/company/:id', getCompany)
+
+companyRouter.get("/companies", getAllCompanies);git 
