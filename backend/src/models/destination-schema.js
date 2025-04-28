213de8c2658d21c3f2b1f2dd287617c5ad7f9dd2
24LP2287 24LP2287 <24LP2287@G301-CC0192.local>
2025-04-28 11:52:47 +0800

    destination backend chip changes

--- backend/src/models/destination-schema.js
+++ backend/src/models/destination-schema.js
@@ -1,16 +1,14 @@
-import mongoose, { SchemaType } from "mongoose";
+import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
 const destinationSchema = new Schema({
-    name: { type: String, required: true },
-    company: { type: Schema.Types.ObjectId, required: true, ref: "Company" }, 
-    description: { type: String, required: true },
-    destination : { type: String, required: true },
-    vibesAvailable: [{ type: String }],
-    cost: { type: String, required: true },
-    image: { type: String, required: true }
-  });
-  
+  name: { type: String, required: true },
+  company: { type: Schema.Types.ObjectId, required: true, ref: "Company" },
+  description: { type: String, required: true },
+  cost: { type: String, required: true },
+  vibesAvailable: [{ type: String }],
+  image: { type: String, required: true }
+});
 
 export const DestinationModel = model("Destination", destinationSchema);
