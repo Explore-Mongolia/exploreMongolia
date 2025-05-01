74639616f2ed8a16889c7f46056d0e3c4de78897
Second <drewuncle863724lu@gmail.com>
2025-05-01 23:20:47 +0800

    algolia on destinations

---
+++ frontenduser/lib/Algolia.ts
@@ -0,0 +1,9 @@
+// import algoliasearch from "algoliasearch/lite";
+
+// const searchClient = algoliasearch(
+//   "ER7KTK9CBD", 
+//   "34f89de5415413cc1b4078e83e7fd16b" 
+// );
+
+// export const destinationsIndex = searchClient.initIndex("destinations_index");
+// export default searchClient; 
