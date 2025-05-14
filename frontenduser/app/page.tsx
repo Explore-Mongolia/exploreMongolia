e1ec56d418f27a4808441bd32fa7d4bd7530760e
Second <drewuncle863724lu@gmail.com>
2025-05-14 21:48:27 +0800

    landing page

--- frontenduser/app/page.tsx
+++ frontenduser/app/page.tsx
@@ -1,25 +1,9 @@
-import { NavbarDemo } from "@/components/Navbar";
-import TripPlannerForm from "@/components/GenerateTrip";
-import ExperienceList from "@/components/ExperienceSection";
-import TripSection from "@/components/TripSection";
-import Hero from "@/components/Hero";
-import FloatingHoverActions from "@/components/FloatingButton";
+import LandingPage from "./landingPage/page";
 
-export default function Home() {
-  return (
-    <div className="relative min-h-screen">
-      <NavbarDemo />
-      <Hero />
-      <div id="experiences">
-        <ExperienceList />
-      </div>
-      <div id="planner">
-        <TripPlannerForm />
-      </div>
-      <TripSection />
-      <div className="fixed bottom-4 left-1/2 -translate-x-1/2 md:left-4 md:translate-x-0 p-4 z-50">
-        <FloatingHoverActions/>
-      </div>
-    </div>
-  );
+const MainPage = () => {
+  return(
+    <LandingPage/>
+  )
 }
+
+export default MainPage;
--- frontenduser/middleware.ts
+++ frontenduser/middleware.ts
@@ -5,23 +5,24 @@ export default clerkMiddleware(async (auth, req) => {
   const { pathname } = req.nextUrl;
   const { userId } = await auth();
 
-  // If the user is not logged in, and the current path is not '/sign-in'
-  if (!userId && pathname !== '/sign-in') {
-    const signInUrl = new URL("/sign-in", req.url);
-    signInUrl.searchParams.set('redirect', pathname); 
-    signInUrl.searchParams.set('message', 'Please log in to access this page.');
+  
+  const publicPaths = ["/", "/sign-in", "/sign-up"];
 
-    // Redirect to the sign-in page
+  if (!userId && !publicPaths.includes(pathname)) {
+    const signInUrl = new URL("/sign-in", req.url);
+    signInUrl.searchParams.set("redirect", pathname);
+    signInUrl.searchParams.set("message", "Please log in to access this page.");
     return NextResponse.redirect(signInUrl);
   }
 
-  // If the user is logged in or already on the sign-in page, proceed with the request
   return NextResponse.next();
 });
 
+
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/((?!_next|.*\\.(?:.*)|sign-in(?:/.*)?|sign-up(?:/.*)?|api/auth|favicon.ico).*)",
     "/(api|trpc)(.*)",
   ],
 };
+
